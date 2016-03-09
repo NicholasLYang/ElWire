e7842e32cc6ddb8b57f27b534f359843c3336ac4
bleepbloopsify <bleepbloopsify@gmail.com>
2016-03-09 12:23:22 -0500

    Johnny

--- withBynn/Johnny/Johnny.ino
+++ withBynn/Johnny/Johnny.ino
@@ -42,5 +42,19 @@ void strobeParts(int cycles, int start){
 }
 
 void loop(){
+  delay(3577 + 377);
+  allOn(1564 + 841);
+  strobeFull(2874 / 50);
+  delay(24);
+  allOn(6352 + 200 + 200 + 200);
+  allOff(200 + 967 + 3210 + 1175);
+  allOn(100);
+  allOff(223 + 100 + 284);
+  strobeParts(4141 / 200, 2);
+  delay(141);
+  allOff(705);
+  allOn(493 + 4671 + 6249);
+  strobeFull( (3724 + 523) / 50);
+  allOff(20);
   delay(60000);//restarts after 1 min
 }
