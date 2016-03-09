af739845d20753357770ecf469f46b069ad8058f
bleepbloopsify <bleepbloopsify@gmail.com>
2016-03-09 14:27:44 -0500

    pls

--- withBynn/Clement/Clement.ino
+++ withBynn/Clement/Clement.ino
@@ -41,5 +41,22 @@ void strobeParts(int cycles, int start){
   }
 }
 void loop(){
+  delay(3577 + 377);
+  allOn(1564 + 841);
+  strobeFull(2874 / 50);
+  delay(24);
+  allOn(6352 + 200);
+  allOff(200 + 200 + 200 + 967);
+  allOn(3210);
+  allOff(1175);
+  alOn(100);
+  allOff(223 + 100 + 284);
+  strobeParts(4141 / 500, 2);
+  allOff(705);
+  allOn(493 + 4671 + 6249);
+  strobeFull((3724 + 523) / 200);
+  allOn(120000);//Skateboard???
+  allOff(2000);
+    
   delay(60000);//restarts after 1 min
 }
